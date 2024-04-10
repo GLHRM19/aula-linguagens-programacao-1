b6524a5c0043e4822e478c6d678573e8cc2186e1
Guilherme da Silva Gonçalves <neimarvalentim.tech@gmail.com>
2024-04-09 21:36:29 -0400

    troca de letra de nome neimar/guilherme

---
+++ j.js
@@ -0,0 +1,8 @@
+usuario = input('Informe seu nome: ')
+x = int(input('posição da letra: '))
+if usuario[x]=='r':
+print(usuario[:x],'s',usuario[x+1:],sep='')
+elif usuario[x]=='m':
+print(usuario[:x], 'n', usuario[x + 1:], sep='')
+else:
+print(usuario[:x], usuario[x + 1:], sep='')
